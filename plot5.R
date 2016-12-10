6e9e3ee3dbb8ddb3824af7d58bba7e158bed499f
fire-elf <fire-elf@users.noreply.github.com>
2016-12-10 18:07:49 -0500

    Add files via upload

--- plot5.R
+++ plot5.R
@@ -24,34 +24,3 @@ dev.off()
 
 # Conclusion
 print("Based on the total emissions calculations for Baltimore, the motor vehicle (on-road) emissions have gone down from 1999 to 2008.")
-
-
-# Based on Short.Names, extract the names that match coal or Coal
-coalSN <- SCC[grep("[Cc]oal", SCC$Short.Name),]
-
-# Based on Short.Names, extract the names that match comb or Comb
-# Initally tried Combust but some entries were omitted that should not have been
-combustSN <-SCC[grep("[Cc]omb", SCC$Short.Name),]
-
-# Pick out only those names that contain both C/coal AND C/comb
-both <- intersect(coalSN$SCC, combustSN$SCC)
-
-# Extract the rows of SCC where the code matches both (coal + combust) 
-# myscc <- subset(SCC,SCC %in% both)
-
-# Based on the SCC codes for the coal+combustion subset, extract/subset rows from NEI
-NEI_4 <- subset(NEI, NEI$SCC %in% both)
-
-# Calculate sum of Emissions for each year
-NEIsums<- tapply(NEI_4$Emissions, NEI_4$year, sum)
-df <- data.frame(c("1999", "2002", "2005", "2008"), NEIsums)
-names(df) <- c("year", "PM25")
-
-# Plot a barplot of the Total Emissions for each Year using png device
-png(file = "plot4.png")
-barplot(df$PM25, col = "purple", xlab = "Year", ylab = "Total Emission (tons)", main = paste("US Coal Combustion Emissions"))
-dev.off()
-
-
-# Conclusion
-print("Across the US, the emissions from coal combustion decreased from 1999 to 2008.")
